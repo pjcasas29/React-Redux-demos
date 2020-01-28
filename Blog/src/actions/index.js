6961d6f4a12147e7c44ff433c44d4107b27d5db2
Pedro Casas <pjcasas29@hotmail.com>
2020-01-27 20:52:59 -0500

    Blog - added authors without multiple fetches

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -1,6 +1,12 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+export const fetchPosts = () => async (dispatch, getState) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({
@@ -8,3 +14,18 @@ export const fetchPosts = () => async dispatch => {
     payload: response.data
   });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder(`/users/${id}`);
+
+  dispatch({type: "FETCH_USER", payload: response.data});
+};
+
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder(`/users/${id}`);
+//
+//   dispatch({type: "FETCH_USER", payload: response.data});
+// });

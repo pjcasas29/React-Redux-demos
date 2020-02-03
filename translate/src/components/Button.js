e79631a6d7b5f40f61b090900499c8db96781355
pjcasas29 <pjcasas29@hotmail.com>
2020-02-03 15:34:01 -0500

    COmponents

---
+++ translate/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button className="ui button primary">Sumbit</button>
+      </div>
+    );
+  }
+}
+export default Button;

6c8083ee21d84f156f40ab8f57fbd2e6b291737f
pjcasas29 <pjcasas29@hotmail.com>
2020-02-03 18:54:13 -0500

    Context added

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,10 +1,17 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div>
-        <button className="ui button primary">Sumbit</button>
+        <button className="ui button primary">
+          <LanguageContext.Consumer>
+            {value => (value === "english" ? "Submit" : "Voorleggen")}
+          </LanguageContext.Consumer>
+        </button>
       </div>
     );
   }
